51b55e1c47a1959b1a29d399ca9fcd4fb88d9626
Silas <silas@level2d.com>
2018-05-11 20:51:44 -0400

    add fs-extra. get rid of system info and default stylings. if file does not exist, correctly copy it over to appdata.

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,42 +1,24 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
     <main>
-      <div class="left-side">
-        <span class="title">
+      <div class="title">
+        <span>
           Genetics - A highly configurable password generator
         </span>
-        <system-information></system-information>
       </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
+      <div class="generator">
           <word-reader></word-reader>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
       </div>
     </main>
   </div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
   import WordReader from './WordReader'
 
   export default {
     name: 'landing-page',
-    components: { SystemInformation, WordReader },
+    components: { WordReader },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
@@ -45,7 +27,7 @@
   }
 </script>
 
-<style>
+<style lang="scss">
   @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 
   * {
@@ -68,63 +50,42 @@
     width: 100vw;
   }
 
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
-
   main {
     display: flex;
     justify-content: space-between;
   }
 
-  main > div { flex-basis: 50%; }
-
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
+  .title span {
     color: #2c3e50;
     font-size: 20px;
     font-weight: bold;
     margin-bottom: 6px;
+    &.alt {
+      font-size: 18px;
+      margin-bottom: 10px;
+    }
   }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
+  .generator {
+    p {
+      color: black;
+      margin-bottom: 10px;
+    }
+    button {
+      font-size: .8em;
+      cursor: pointer;
+      outline: none;
+      padding: 0.75em 2em;
+      border-radius: 2em;
+      display: inline-block;
+      color: #fff;
+      background-color: #4fc08d;
+      transition: all 0.15s ease;
+      box-sizing: border-box;
+      border: 1px solid #4fc08d;
+    }
+    button.alt {
+      color: #42b983;
+      background-color: transparent;
+    }
   }
 </style>
--- src/renderer/components/WordReader.vue
+++ src/renderer/components/WordReader.vue
@@ -6,7 +6,8 @@
 </template>
 
 <script>
-import fs from 'fs'
+import fs from 'fs-extra'
+import path from 'path'
 import readline from 'readline'
 import { remote } from 'electron'
 
@@ -31,21 +32,45 @@ export default {
     },
     ReadLine (index) {
       var vm = this
+      var dataPath = path.join(remote.app.getPath('appData'), 'Genetics/english.txt')
       if (vm.filepath === '' || vm.filepath === null) {
-        console.log('no file opened')
-        return
+        // time to read the default english dict
+        vm.filepath = dataPath
+        var dataReadStream = fs.createReadStream(dataPath)
+
+        dataReadStream.on('error', (readError) => {
+          // We must not have the file!
+          fs.copy(path.join(__static, 'english.txt'), dataPath).then(() => {
+            console.log('copied over the english dict')
+            vm.ReadLine(index)
+          }).catch(err => console.error(err))
+        })
+        dataReadStream.on('readable', () => {
+          // we have the file!
+          let done = false
+          while (!done) {
+            if (dataReadStream.read() == null) {
+              done = true
+            }
+          }
+        })
+        dataReadStream.on('end', () => {
+          vm.ReadLine(index)
+        })
+      } else {
+        var instream = fs.createReadStream(vm.filepath)
+        var rl = readline.createInterface(instream)
+        console.log('we have the file')
+        rl.on('line', (line) => {
+          if (vm.lineCounter++ === index) {
+            console.log(`Line read: ${line}`)
+          }
+        })
+        rl.on('close', () => {
+          vm.lineCounter = 0
+          console.log('done reading')
+        })
       }
-      var instream = fs.createReadStream(vm.filepath)
-      var rl = readline.createInterface(instream)
-      rl.on('line', (line) => {
-        if (vm.lineCounter++ === index) {
-          console.log(`Line read: ${line}`)
-        }
-      })
-      rl.on('close', () => {
-        vm.lineCounter = 0
-        console.log('done reading')
-      })
     }
   }
 }
